a72c32aefe2ed1648adcc140195d20a5728bc5d5
Edilson Bitencourt <theusdido@hotmail.com>
2023-08-29 08:42:12 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public image_add:string = 'assets/icon/image-add.jpg';
   constructor() {}
 
+  abrirCamera(){
+    const takePicture = async () => {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri,
+        source:CameraSource.Camera
+      });
+    
+      // image.webPath will contain a path that can be set as an image src.
+      // You can access the original file using image.path, which can be
+      // passed to the Filesystem API to read the raw data of the image,
+      // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+      var imageUrl = image.webPath;
+    
+      // Can be set to the src of an image now
+      this.image_add = String(imageUrl);
+    };    
+    takePicture();
+  }
 }
